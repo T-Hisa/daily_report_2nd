21a6da20df999e217fb99631429b3652469e9316
Takaaki <dacchin.h081229@gmail.com>
2021-12-26 11:43:25 +0900

    commit

--- actions/dummy_actions.js
+++ actions/dummy_actions.js
@@ -1,13 +1,17 @@
 const { app } = require('../app')
 
-app.action("time", async ({ ask }) => {
+app.action("time", async ({ ack }) => {
   ack()
 })
 
-app.action("comment", async ({ ask }) => {
+app.action("comment", async ({ ack }) => {
   ack();
 });
 
-app.action("status", async ({ ask }) => {
+app.action("status", async ({ ack }) => {
+  ack();
+});
+
+app.action("activity", async ({ ack }) => {
   ack();
 });
